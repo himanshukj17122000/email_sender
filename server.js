5ea255dbdd4e993534a819a1fc827c38a96dca08
Himanshu Jain <hkj4@dj4.edu>
2020-12-06 07:43:46 -0500

    added cors

--- server.js
+++ server.js
@@ -3,12 +3,21 @@ const bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
 const PORT = process.env.PORT || 5000;
 var transporter = nodemailer.createTransport({
-    service: 'gmail', //process.env.SERVICE_PROVIDER,
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, 
     auth: {
         user: `${process.env.SENDER}`,
         pass: `${process.env.PASSWORD}`
     }
 });
+// var transporter = nodemailer.createTransport({
+//     service: 'gmail', //process.env.SERVICE_PROVIDER,
+//     auth: {
+//         user: `${process.env.SENDER}`,
+//         pass: `${process.env.PASSWORD}`
+//     }
+// });
 // create express app
 const app = express();
 
@@ -27,7 +36,6 @@ app.post('/', async (req, res) => {
         html: `<p>Welcome to the app, Himanshu! Let me know how you get along with the app. </p>`, // plain text body
         replyTo: process.env.SENDER
     };
-
     transporter.sendMail(mailOptions, function (err, info) {
         if (err)
             console.log(err)
